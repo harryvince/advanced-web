a75aedc7746270414e73bff67e4a36ab42eb676a
Harry Vince <harryavince@gmail.com>
2022-06-17 19:18:26 +0100

    db connection built but not implemented

---
+++ src/controller/db.php
@@ -0,0 +1,28 @@
+<?php 
+
+    class DB {
+    
+        private static $writeDBConnection;
+        private static $readDBConnection;
+    
+        public static connectWriteDB() {
+            if (self::$writeDBConnection === null) {
+                self::$writeDBConnection = new PDO('mysql:host=database;dbname=api;charset=utf-8', 'root', 'mauFJcuf5dhRMQrjj');
+                self::$writeDBConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                self::$writeDBConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            }
+            return self::$writeDBConnection;
+        }
+    
+        public static connectReadDB() {
+            if (self::$readDBConnection === null) {
+                self::$readDBConnection = new PDO('mysql:host=database;dbname=api;charset=utf-8', 'root', 'mauFJcuf5dhRMQrjj');
+                self::$readDBConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                self::$readDBConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            }
+            return self::$readDBConnection;
+        }
+    }
+
+
+?>
