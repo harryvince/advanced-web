1496eb5e6c5973d333994a395e9aedfe1133d073
Harry Vince <harryavince@gmail.com>
2022-06-18 23:58:31 +0100

    Created post endpoint

--- src/controller/task.php
+++ src/controller/task.php
@@ -142,6 +142,15 @@ if(array_key_exists("taskid", $_GET)) {
             $rowCount = $query->rowCount();
             $taskArray = array();
 
+            if ($rowCount === 0) {
+                $response = new Response();
+                $response->setHttpStatusCode(404);
+                $response->setSuccess(false);
+                $response->addMessage("Tasks Not Found");
+                $response->send();
+                exit;
+            }
+
             while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
                 $task = new Task($row['id'], $row['title'], $row['description'], $row['date'], $row['start_time'], $row['end_time'], $row['deadline'], $row['complete']);
                 $taskArray[] = $task->getTasksAsArray();
@@ -173,24 +182,147 @@ if(array_key_exists("taskid", $_GET)) {
             $response->send();
             exit;
         } 
-    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    
     } else {
         $response = new Response();
         $response->setHttpStatusCode(405);
         $response->setSuccess(false);
         $response->addMessage("Error: Invalid Request Method");
         $response->send();
-    exit;
+        exit;
     } 
 
-} else {
-    $response = new Response();
-    $response->setHttpStatusCode(404);
-    $response->setSuccess(false);
-    $response->addMessage("Error: Invalid Endpoint");
-    $response->send();
-    exit;
+} elseif (empty($_GET)) {
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        try {
+            if($_SERVER['CONTENT_TYPE'] !== 'application/json') {
+                $response = new Response();
+                $response->setHttpStatusCode(415);
+                $response->setSuccess(false);
+                $response->addMessage("Error: Unsupported Content Type Header");
+                $response->send();
+                exit;
+            }
+            
+            $rawPOSTData = file_get_contents('php://input');
+
+            if(!$jsonData = json_decode($rawPOSTData)) {
+                $response = new Response();
+                $response->setHttpStatusCode(400);
+                $response->setSuccess(false);
+                $response->addMessage("Error: Request Body is not Valid JSON");
+                $response->send();
+                exit;
+            }
+
+            if(!isset($jsonData->title) || !isset($jsonData->complete)) {
+                $response = new Response();
+                $response->setHttpStatusCode(400);
+                $response->setSuccess(false);
+                (!isset($jsonData->title) ? $response->addMessage("Error: Title is a Mandatory Field") : false);
+                (!isset($jsonData->complete) ? $response->addMessage("Error: Complete Status is a Mandatory Field") : false);
+                $response->send();
+                exit;
+            }
+
+            $newTask = new Task(null,
+                                (isset($jsonData->title) ? $jsonData->title : null),
+                                (isset($jsonData->description) ? $jsonData->description : null),
+                                (isset($jsonData->date) ? $jsonData->date : null),
+                                (isset($jsonData->start_time) ? $jsonData->start_time : null),
+                                (isset($jsonData->end_time) ? $jsonData->end_time : null),
+                                (isset($jsonData->deadline) ? $jsonData->deadline : null),
+                                (isset($jsonData->complete) ? $jsonData->complete : null));
+            $title = $newTask->getTitle();
+            $description = $newTask->getDescription();
+            $date = $newTask->getDate();
+            $start_time = $newTask->getStartTime();
+            $end_time = $newTask->getEndTime();
+            $deadline = $newTask->getDeadline();
+            $complete = $newTask->getComplete();
+
+            $query = $writeDB->prepare('insert into tbl_tasks (title, description, date, start_time, end_time, deadline, complete) values (:title, :description, STR_TO_DATE(:date, \'%d-%m-%Y\'), :start_time, :end_time, STR_TO_DATE(:deadline, \'%d-%m-%Y %H:%i\'), :complete)');
+
+            $query->bindParam(':title', $title, PDO::PARAM_STR);
+            $query->bindParam(':description', $description, PDO::PARAM_STR);
+            $query->bindParam(':date', $date, PDO::PARAM_STR);
+            $query->bindParam(':start_time', $start_time, PDO::PARAM_STR);
+            $query->bindParam(':end_time', $end_time, PDO::PARAM_STR);
+            $query->bindParam(':deadline', $deadline, PDO::PARAM_STR);
+            $query->bindParam(':complete', $complete, PDO::PARAM_STR);
+
+            $query->execute();
+
+            $rowCount = $query->rowCount();
+
+            if($rowCount === 0) {
+                $response = new Response();
+                $response->setHttpStatusCode(500);
+                $response->setSuccess(false);
+                $response->addMessage("Error: Failed to Insert Task into Database");
+                $response->send();
+                exit;
+            }
+
+            $lastTaskID = $writeDB->lastInsertID();
+
+            $query = $readDB->prepare('select id, title, description, DATE_FORMAT(date, "%d-%m-%Y") as "date", start_time, end_time, DATE_FORMAT(deadline, "%d-%m-%Y %H:%i") as "deadline", complete from tbl_tasks where id = :taskid');
+            $query->bindParam(':taskid', $lastTaskID, PDO::PARAM_INT);
+            $query->execute();
+
+            $rowCount = $query->rowCount();
+            $taskArray = array();
+
+            if ($rowCount === 0) {
+                $response = new Response();
+                $response->setHttpStatusCode(404);
+                $response->setSuccess(false);
+                $response->addMessage("Error: Task ID Not Found");
+                $response->send();
+                exit;
+            }
+
+            while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+                $task = new Task($row['id'], $row['title'], $row['description'], $row['date'], $row['start_time'], $row['end_time'], $row['deadline'], $row['complete']);
+                $taskArray[] = $task->getTasksAsArray();
+            }
+
+            $returnData = array();
+            $returnData['rows_returned'] = $rowCount;
+            $returnData['tasks'] = $taskArray;
+
+            $response = new Response();
+            $response->setHttpStatusCode(200);
+            $response->setSuccess(true);
+            $response->toCache(true);
+            $response->setData($returnData);
+            $response->send();
+            exit;
+        } catch (TaskException $exception) {
+            $response = new Response();
+            $response->setHttpStatusCode(400);
+            $response->setSuccess(false);
+            $response->addMessage($exception->getMessage());
+            $response->send();
+            exit;
+        } catch (PDOException $exception) {
+            error_log("Database Query Error: ".$exception, 0);
+            $response = new Response();
+            $response->setHttpStatusCode(500);
+            $response->setSuccess(false);
+            $response->addMessage("Error: Failed to Insert Task into Database");
+            $response->send();
+            exit;
+        }
+    }
+    else {
+        $response = new Response();
+        $response->setHttpStatusCode(404);
+        $response->setSuccess(false);
+        $response->addMessage("Error: Invalid Endpoint");
+        $response->send();
+        exit;
+    } 
 }
     
 ?>
